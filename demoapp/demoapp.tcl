6818b5fb4d5bc57a3c0133bbe67d929f7045c7b2
philippose <philippose@38e36fca-e102-11dd-b6c5-65a6809f2f0c>
2009-04-05 18:33:32 +0000

    * Simpler external application loading * Library loading done in application TCL * ng.tcl only sources application TCL script file
    git-svn-id: https://netgen-mesher.svn.sourceforge.net/svnroot/netgen-mesher@135 38e36fca-e102-11dd-b6c5-65a6809f2f0c

--- demoapp/demoapp.tcl
+++ demoapp/demoapp.tcl
@@ -1,20 +1,18 @@
-if { [catch { load libdemoapp.so demoapp }] } {
+if { [catch { load libdemoapp[info sharedlibextension] demoapp } result] } {
     puts "cannot load demoapp" 
-}
+	puts "error: $result"
+} {
+    puts "loading demo-application"
 
-.ngmenu add cascade -label "DemoApp" -menu .ngmenu.demo -underline 0
+    .ngmenu add cascade -label "DemoApp" -menu .ngmenu.demo -underline 0
  
-menu .ngmenu.demo
-.ngmenu.demo add command -label "Choose me" \
-    -command { DA_ChooseMe }
+    menu .ngmenu.demo
+    .ngmenu.demo add command -label "Choose me" \
+        -command { DA_ChooseMe }
 
-.ngmenu.demo add command -label "Print Mesh" \
-    -command { DA_PrintMesh }
+    .ngmenu.demo add command -label "Print Mesh" \
+        -command { DA_PrintMesh }
 
-.ngmenu.demo add command -label "Compute Solution" \
-    -command { DA_SetSolution }
-
-
-
-puts "loading demo-application"
-
+    .ngmenu.demo add command -label "Compute Solution" \
+        -command { DA_SetSolution }
+}
