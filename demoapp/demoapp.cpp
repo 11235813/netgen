b583f8d1635182373aae58b6e60eec79acfb0e58
philippose <philippose@38e36fca-e102-11dd-b6c5-65a6809f2f0c>
2009-04-05 18:31:48 +0000

    * Streamlined code of demonstration application * Added a header file for code clarity * Added Windows MSVC 2008 / 2005 Project files * Required Makefile.am modifications made
    git-svn-id: https://netgen-mesher.svn.sourceforge.net/svnroot/netgen-mesher@134 38e36fca-e102-11dd-b6c5-65a6809f2f0c

--- demoapp/demoapp.cpp
+++ demoapp/demoapp.cpp
@@ -1,6 +1,11 @@
-/*
-  This is a demo for a NG Appication programme
- */
+// ------------------------------------------------------------------
+/*!
+   \file demoapp.c
+   \brief Demonstration application for a Netgen Application Program
+   \author Joachim Schoeberl
+   \date 
+*/
+// ------------------------------------------------------------------
 
 
 #include <iostream>
@@ -10,8 +15,40 @@ using namespace std;
 #include <tcl.h>
 
 // netgen interface
-#include "/opt/netgen/include/nginterface.h"
+#include "nginterface.h"
 
+// Include the local header for declaration of locally 
+// exported functions
+#include "demoapp.h"
+
+
+// ****** Local function declarations ******
+int DA_ChooseMe (ClientData clientData, Tcl_Interp * interp, int argc, const char *argv[]);
+int DA_PrintMesh (ClientData clientData, Tcl_Interp * interp, int argc, const char *argv[]);
+int DA_SetSolution (ClientData clientData, Tcl_Interp * interp, int argc, const char *argv[]);
+
+// ****** Start of Code ******
+
+// Initialisation function for Netgen -> Application interface 
+// through TCL
+int Demoapp_Init (Tcl_Interp * interp)
+{
+  cout << "Init demoapp" << endl;
+
+  Tcl_CreateCommand (interp, "DA_ChooseMe", DA_ChooseMe,
+		     (ClientData)NULL,
+		     (Tcl_CmdDeleteProc*) NULL);
+
+  Tcl_CreateCommand (interp, "DA_PrintMesh", DA_PrintMesh,
+		     (ClientData)NULL,
+		     (Tcl_CmdDeleteProc*) NULL);
+
+  Tcl_CreateCommand (interp, "DA_SetSolution", DA_SetSolution,
+		     (ClientData)NULL,
+		     (Tcl_CmdDeleteProc*) NULL);
+
+  return TCL_OK;
+}
 
 
 
@@ -22,6 +59,8 @@ int DA_ChooseMe (ClientData clientData,
   cout << "Hi" << endl;
   return TCL_OK;
 }
+
+
 
 int DA_PrintMesh (ClientData clientData,
 		  Tcl_Interp * interp,
@@ -56,6 +95,7 @@ int DA_PrintMesh (ClientData clientData,
 }
 
 
+
 int DA_SetSolution (ClientData clientData,
 		    Tcl_Interp * interp,
 		    int argc, const char *argv[])
@@ -86,33 +126,6 @@ int DA_SetSolution (ClientData clientData,
   soldata.name = "My Solution";
 
   Ng_SetSolutionData (&soldata);
-
-  return TCL_OK;
-}
-
-#ifdef WIN32
-#define DLL_HEADER __declspec(dllimport)
-#endif
-
-
-// initialize Tcl commands
-extern "C" DLL_HEADER int Demoapp_Init (Tcl_Interp * interp);
-
-int Demoapp_Init (Tcl_Interp * interp)
-{
-  cout << "Init demoapp" << endl;
-
-  Tcl_CreateCommand (interp, "DA_ChooseMe", DA_ChooseMe,
-		     (ClientData)NULL,
-		     (Tcl_CmdDeleteProc*) NULL);
-
-  Tcl_CreateCommand (interp, "DA_PrintMesh", DA_PrintMesh,
-		     (ClientData)NULL,
-		     (Tcl_CmdDeleteProc*) NULL);
-
-  Tcl_CreateCommand (interp, "DA_SetSolution", DA_SetSolution,
-		     (ClientData)NULL,
-		     (Tcl_CmdDeleteProc*) NULL);
 
   return TCL_OK;
 }
---
+++ demoapp/demoapp.h
@@ -0,0 +1,34 @@
+#ifndef DEMOAPP
+#define DEMOAPP
+
+/**************************************************************************/
+/* File:   demoapp.h
+/* Author: Joachim Schoeberl
+/* Date:   
+/**************************************************************************/
+
+/*!
+   \file demoapp.h
+   \brief Header - Demonstration application for a Netgen Application Program
+   \author Joachim Schoeberl
+   \date 
+*/
+
+// DLL Import Headers for Netgen functions
+
+#ifdef WIN32
+   #define LOCAL_EXPORTS __declspec(dllexport)
+#else
+   #define LOCAL_EXPORTS 
+#endif
+
+
+// ****** Functions exported by the Application ******
+// initialize Tcl commands
+// Must always be <appname>_Init(Tcl_Interp*)
+extern "C" LOCAL_EXPORTS int Demoapp_Init (Tcl_Interp * interp);
+
+
+// ****** End of Module ******
+#endif // #ifndef DEMOAPP
+
